e2ac3a1c5baf79217acecc3e0241aa3a131aa8dd
KalluruS <>
2020-03-21 10:53:16 +0530

    --- Auto-Git Commit ---

--- nuke-ami.sh
+++ nuke-ami.sh
@@ -4,5 +4,5 @@ read -p 'Enter which region [A-All]:' region
 
 case $region in 
   A|a) 
-    region=(us-east-1 us-east-2 us-west-1 us-west-2 ap-east-1 ap-south-1)
+    region=(us-east-1 us-east-2 us-west-1 us-west-2 ap-east-1 ap-south-1 ap-northeast)
 
