feb0dce714f98c12c56111bc45ff84a5b4076774
KalluruS <>
2020-03-21 11:01:17 +0530

    --- Auto-Git Commit ---

--- nuke-ami.sh
+++ nuke-ami.sh
@@ -2,7 +2,7 @@
 
 ALL_REGIONS="us-east-1 us-east-2 us-west-1 us-west-2 ap-east-1 ap-south-1 ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 me-south-1 sa-east-1"
 
-echo -e "\e[33mAWS REGIONS : $ALL_REGIONS\e[0m"
+echo -e "\n\e[33mAWS REGIONS : $ALL_REGIONS\e[0m\n"
 read -p 'Enter which region [A-All]:' region 
 
 case $region in 
