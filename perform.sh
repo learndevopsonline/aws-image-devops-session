c3c62a78d7833eab446fa63666c726acc1b52658
KalluruS <>
2020-10-24 06:38:57 +0530

    ss

---
+++ perform.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REGIONS=(us-east-1 us-east-2 us-west-2)
+
+for REGION in ${REGIONS[*]} ; do 
+  aws="aws --region $REGION --profile cit"
+  AMIID=$($aws ec2 describe-images --owners 973714476881 --query 'Images[*].{ID:ImageId}' --output text)
+  if [ -n "$AMIID" ]; then
+    $aws ec2 deregister-image --image-id $AMIID 
+  fi
+  for snap in `$aws ec2 describe-snapshots --owner-ids 973714476881 --filters Name=tag:Created_By,Values=Packer --query "Snapshots[*].{ID:SnapshotId}" --output text ` ; do 
+    $aws ec2 delete-snapshot --snapshot-id $snap
+  done
+done
+#packer build aws.json
+
