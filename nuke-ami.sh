691df7ea83ec79e3df6a642741ba7a12d4752f0d
KalluruS <>
2021-03-27 10:25:50 +0530

    ss

--- nuke-ami.sh
+++ nuke-ami.sh
@@ -1,9 +1,14 @@
 #!/bin/bash 
 
+## Set AWS Profile 
+
+~/bin/aws-profile.sh set
+
 ALL_REGIONS="us-east-1 us-east-2 us-west-1 us-west-2 ap-east-1 ap-south-1 ap-northeast-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 me-south-1 sa-east-1"
 
-echo -e "\n\e[33mAWS REGIONS : $ALL_REGIONS\e[0m\n"
-read -p 'Enter which region [A-All]:' region 
+echo "\n\033[1;33mAWS REGIONS : $ALL_REGIONS\033[0m\n"
+#read -p 'Enter which region [A-All]:' region 
+region=us-east-1
 
 case $region in 
   A|a) 
@@ -18,9 +23,18 @@ case $region in
     ;;
 esac
 
-for REGION in ${region[*]}; do 
-  echo -n "Checking in Region - $REGION" - 
-  
-done 
-
+for REGION in ${region[*]} ; do 
+  aws="aws --region $REGION"
+  AMIID=$($aws ec2 describe-images --owners 973714476881 --query 'Images[*].{ID:ImageId}' --output text)
+  if [ -n "$AMIID" ]; then
+    echo "Found AMI in $REGION & AMIID is $AMIID" 
+    $aws ec2 deregister-image --image-id $AMIID | jq
+  else 
+    echo "No Image found in $REGION"
+  fi
+  for snap in `$aws ec2 describe-snapshots --owner-ids 973714476881 --filters Name=tag:Created_By,Values=Packer --query "Snapshots[*].{ID:SnapshotId}" --output text ` ; do 
+    echo "Deleting SnapShot $snap in $REGION region"
+    $aws ec2 delete-snapshot --snapshot-id $snap | jq
+  done 
+done
 
