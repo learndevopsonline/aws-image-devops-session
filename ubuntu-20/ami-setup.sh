40cee7cac6eca0db4483b39c232334950da14de0
KalluruS <>
2021-04-04 13:05:37 +0530

    ss

--- ubuntu-20/ami-setup.sh
+++ ubuntu-20/ami-setup.sh
@@ -19,8 +19,6 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 
-## Install Base Packages
-yum -y install https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
 PACK_LIST="zip unzip make net-tools jq"
 info "Installing Base Packages"
 for package in $PACK_LIST ; do 
@@ -64,19 +62,15 @@ chattr +i /etc/ssh/sshd_config
 Stat $? "Enable Password Login"
 
 ## Setup user passwords
-ROOT_PASS="DevOps321"
-UBUNTU_PASS="DevOps321"
-#usermod -a -G google-sudoers centos &>/dev/null
-# echo "echo $ROOT_PASS | passwd --stdin root"   >>/etc/rc.d/rc.local 
-# echo "echo $CENTOS_PASS | passwd --stdin centos"   >>/etc/rc.d/rc.local 
-# echo "sed -i -e 's/^centos:!!/centos:/' /etc/shadow" >>/etc/rc.d/rc.local
-# Stat $? "Setup Password for Users"
+PASS="DevOps321"
+PASS="DevOps321"
+echo -e "usermod -p DevOps321 root\nusermod -p DevOps321 ubuntu"   >>/etc/rc.d/rc.local 
+echo "sed -i -e 's/^ubuntu:!!/ubuntu:/' /etc/shadow" >>/etc/rc.d/rc.local
 info "   Following are the Usernames and Passwords"
-Infot "ubuntu / $UBUNTU_PASS"
-Infot "  root / $ROOT_PASS"
+Infot "ubuntu / $PASS"
+Infot "  root / $PASS"
 echo
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIfSCB5MtXe54V3lWGBGSxMWPue5CjmSA4ky7E8GUoeZdXxI+df7msJL93PzmtwU3v+O+NLNJJRfmaGpEkgidVXoi6mnYUVCHb1y4zd6QIFEyglGDlvZ4svhHt7T15B13bJC3mTaR2A/xqlvE0/a4XKN1ATYyn6K6CTFJT8I4TIDQmO3PbcNsNFXoO1ef657aqNf0AXC1QWum3HulIt6iJ4s0pQI4hDTmR5EskJxr2K62F4JDOYmVu8bGhFT6ohYbXBCGQtmdp716RnF0Cp1htmxM001wvCSjWLPZuuBjtHXX+op+MJGr0aIqqxdVZ2gw0JeIDfVo7pkSIdTu+p2Yn devops' >/root/.ssh/authorized_keys
-#sed -i -e 's/showfailed//' /etc/pam.d/postlogin
 chmod +x /etc/rc.d/rc.local 
 systemctl enable rc-local
 
