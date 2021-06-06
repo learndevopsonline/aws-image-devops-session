d92ef2958493d8b28610bb830519cdb131b957dd
KalluruS <>
2021-06-06 21:42:10 +0530

    Update AMI Script for Centos-7

--- centos-7/ami-setup.sh
+++ centos-7/ami-setup.sh
@@ -111,7 +111,7 @@ cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
 unzip awscliv2.zip &>/dev/null
 /tmp/aws/install
-aws --version || true
+/usr/local/bin/aws --version || true
 
 rm -rf /var/lib/yum/*  /tmp/*
 sed -i -e '/aws-hostname/ d' -e '$ a r /tmp/aws-hostname' /usr/lib/tmpfiles.d/tmp.conf
