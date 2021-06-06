167263c1896e3030359a58c34723907f7d7d8678
KalluruS <>
2021-06-06 18:42:37 +0530

    Update AMI Script for Centos-7

--- centos-7/scipts/ps1.sh
+++ centos-7/scipts/ps1.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-export PS1="[ \[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]\h\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\\$ "
+
+HOST_NAME=$1
+if [ -z "$1" ]; then
+  echo -e "Give Hostname as Input"
+  exit 1
+fi
+
+if [ $(id -u) -ne 0 ]; then
+  echo "You should be root user to execute this!!"
+  exit 1
+fi
+
+echo 'export PS1="[ \[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]HOST_NAME\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\\$ "' >/etc/profile.d/ps1.sh
+sed -i -e "s/HOST_NAME/${HOST_NAME}/" /etc/profile.d/ps1.sh
+chmod +x /etc/profile.d/ps1.sh
+
+su - $USER
