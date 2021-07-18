8bc24a461bc18db3046a3519702c03fd3283e59d
KalluruS <>
2021-07-18 08:03:46 +0530

    Fix cleanup of cloned script

--- centos-7/nuke-ami.sh
+++ centos-7/nuke-ami.sh
@@ -28,12 +28,12 @@ for REGION in ${region[*]} ; do
   AMIID=$($aws ec2 describe-images --owners 973714476881 --filters "Name=tag:Name,Values=Centos-7-DevOps-Practice" --query 'Images[*].{ID:ImageId}' --output text)
   if [ -n "$AMIID" ]; then
     echo "Found AMI in $REGION & AMIID is $AMIID"
-    $aws ec2 deregister-image --image-id $AMIID | jq
+    $aws ec2 deregister-image --image-id $AMIID | jq .
   else
     echo "No Image found in $REGION"
   fi
   for snap in `$aws ec2 describe-snapshots --owner-ids 973714476881 --filters Name=tag:Created_By,Values=Packer --query "Snapshots[*].{ID:SnapshotId}" --output text ` ; do
     echo "Deleting SnapShot $snap in $REGION region"
-    $aws ec2 delete-snapshot --snapshot-id $snap | jq
+    $aws ec2 delete-snapshot --snapshot-id $snap | jq .
   done
 done
