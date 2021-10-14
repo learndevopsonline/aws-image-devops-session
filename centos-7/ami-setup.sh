b8c88c67ae08807193512d6dac72d6db69b469c5
R Kalluru <skalluru@local>
2021-10-14 22:30:00 +0530

    ss

--- centos-7/ami-setup.sh
+++ centos-7/ami-setup.sh
@@ -43,6 +43,8 @@ for package in $PACK_LIST ; do
     yum install $package -y &>/dev/null  
     Statt $? "Installed $package"
 done
+
+yum remove mariadb-libs -y &>/dev/null
 yum clean all &>/dev/null 
 
 ## Fixing SSH timeouts
@@ -50,14 +52,17 @@ sed -i -e '/TCPKeepAlive/ c TCPKeepAlive no' -e '/ClientAliveInterval/ c ClientA
 Stat $? "Fixing SSH timeouts"
 
 ## Enable color prompt
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/ps1.sh -o /etc/profile.d/ps1.sh
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/ps1.sh -o /etc/profile.d/ps1.sh
 chmod +x /etc/profile.d/ps1.sh
 Stat $? "Enable Color Prompt"
 
 ## Enable idle shutdown
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/idle.sh -o /boot/idle.sh
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/idle.sh -o /boot/idle.sh
 chmod +x /boot/idle.sh
 STAT1=$?
+
+## Setup Sudoers
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/sudoers >/etc/sudoers
 
 sed -i -e '/idle/ d' /var/spool/cron/root &>/dev/null
 echo "*/10 * * * * sh -x /boot/idle.sh &>/tmp/idle.out" >/var/spool/cron/root
@@ -92,6 +97,7 @@ Infot "centos / $CENTOS_PASS"
 Infot "  root / $ROOT_PASS"
 echo
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIfSCB5MtXe54V3lWGBGSxMWPue5CjmSA4ky7E8GUoeZdXxI+df7msJL93PzmtwU3v+O+NLNJJRfmaGpEkgidVXoi6mnYUVCHb1y4zd6QIFEyglGDlvZ4svhHt7T15B13bJC3mTaR2A/xqlvE0/a4XKN1ATYyn6K6CTFJT8I4TIDQmO3PbcNsNFXoO1ef657aqNf0AXC1QWum3HulIt6iJ4s0pQI4hDTmR5EskJxr2K62F4JDOYmVu8bGhFT6ohYbXBCGQtmdp716RnF0Cp1htmxM001wvCSjWLPZuuBjtHXX+op+MJGr0aIqqxdVZ2gw0JeIDfVo7pkSIdTu+p2Yn devops' >/root/.ssh/authorized_keys
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFoOQSSWSX4iJ1F42FODfS7Ct7wxnzRMuKAoTK67Zd5JkjETvroEOcwJHKeRVbjLT8hZuWMz3JdowR25+7W5N23GaBvBq7HbQwec2UGGA6AFAMmijpY1KDZznfBsqVvMY5yT/4XB1RU78dffRuNUs/IeMYnxoh6UO62Zg33JLtJY6waIFNtCFPTN8m4JrsPlt4s6X8E15Jn9Qh9TDNw+R7piDZ/KRDE+paMkflMpptfcNIbK8kzC9/p3DiAMBjmfrReGueI9vrSN66L/BepPTRoUvv9iavKbmu8DEITETlhGnn79V0r0ekXDE6WgZtnTBbbjSFsilNmLw7xjGMS0Bx root@ip-172-31-15-115.ec2.internal' >>/root/.ssh/authorized_keys
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIfSCB5MtXe54V3lWGBGSxMWPue5CjmSA4ky7E8GUoeZdXxI+df7msJL93PzmtwU3v+O+NLNJJRfmaGpEkgidVXoi6mnYUVCHb1y4zd6QIFEyglGDlvZ4svhHt7T15B13bJC3mTaR2A/xqlvE0/a4XKN1ATYyn6K6CTFJT8I4TIDQmO3PbcNsNFXoO1ef657aqNf0AXC1QWum3HulIt6iJ4s0pQI4hDTmR5EskJxr2K62F4JDOYmVu8bGhFT6ohYbXBCGQtmdp716RnF0Cp1htmxM001wvCSjWLPZuuBjtHXX+op+MJGr0aIqqxdVZ2gw0JeIDfVo7pkSIdTu+p2Yn devops' >/home/centos/.ssh/authorized_keys
 sed -i -e 's/showfailed//' /etc/pam.d/postlogin
 chmod +x /etc/rc.d/rc.local 
@@ -101,15 +107,19 @@ systemctl enable rc-local
 #cat /dev/zero | ssh-keygen -q -N ""
 #cat /root/.ssh/id_rsa.pub >>/root/.ssh/authorized_key
 ## Moved from generting ssh keys to using the ones from repo.
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/id_rsa >/root/.ssh/id_rsa
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/id_rsa.pub >/root/.ssh/id_rsa.pub
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/id_rsa >/root/.ssh/id_rsa
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/id_rsa.pub >/root/.ssh/id_rsa.pub
 chmod 600 /root/.ssh/id_rsa
 chmod 644 /root/.ssh/id_rsa.pub
 chattr +i /root/.ssh/authorized_keys
 echo 'Host *
     User root
-    StrictHostKeyChecking no' >/root/.ssh/config 
-chmod 600 /root/.ssh/config
+    UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no' >/root/.ssh/config
+echo 'Host *
+    UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no' >/home/centos/.ssh/config
+chmod 600 /root/.ssh/config /home/centos/.ssh/config
 
 # Install AWS CLI 
 cd /tmp 
@@ -124,17 +134,17 @@ sed -i -e '/aws-hostname/ d' -e '$ a r /tmp/aws-hostname' /usr/lib/tmpfiles.d/tm
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/labauto >/bin/labauto 
 chmod +x /bin/labauto 
 
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/disable-auto-shutdown >/bin/disable-auto-shutdown
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/disable-auto-shutdown >/bin/disable-auto-shutdown
 chmod +x /bin/disable-auto-shutdown
 
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/enable-auto-shutdown >/bin/enable-auto-shutdown
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/enable-auto-shutdown >/bin/enable-auto-shutdown
 chmod +x /bin/enable-auto-shutdown
 
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/set-hostname >/bin/set-hostname
-#cp scipts/set-hostname /bin
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/set-hostname >/bin/set-hostname
+#cp scripts/set-hostname /bin
 chmod +x /bin/set-hostname
 
-curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scipts/motd >/etc/motd
+curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-7/scripts/motd >/etc/motd
 
 #hint "System is going to shutdown now.. Make a note of the above passwords and save them to use with all your servers .."
 #echo
