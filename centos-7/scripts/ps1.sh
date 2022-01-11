5974fc09cec11d3163afb2caccbd8be7e6fc16d9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-11 06:29:46 +0530

    ss

--- centos-7/scripts/ps1.sh
+++ centos-7/scripts/ps1.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-if [ -z "${PUBLIC_IP}" ]; then
+curl -f -s http://169.254.169.254/latest/meta-data/public-ipv4 &>/dev/null
+if [ $? -eq 0 ]; then
+  PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4 2>/dev/null)
+else
   PUBLIC_IP=null
 fi
-PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
+PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null)
+INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type 2>/dev/null)
 export PS1="
 \e[1;32m${PUBLIC_IP} | ${PRIVATE_IP} | ${INSTANCE_TYPE}
 [ \[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]\h\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\\$ "
