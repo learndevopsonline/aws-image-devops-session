48c6024148abfec5d1f464c293c4f29f24ae5a2c
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-16 15:34:37 +0530

    ss

--- centos-9/ami-setup.sh
+++ centos-9/ami-setup.sh
@@ -47,7 +47,6 @@ Stat $? "Enable Color Prompt"
 curl -s https://raw.githubusercontent.com/linuxautomations/aws-image-devops-session/master/centos-9/scripts/tuptime >/bin/tuptime
 chmod +x /bin/tuptime
 
-rm -f /etc/ssh/sshd_config.d/50-cloud-init.conf
 useradd centos
 sed -i -e '/ec2-user/ d' /etc/passwd /etc/shadow /etc/group /etc/gshadow
 sed -i -e 's/ec2-user/centos/' /etc/sudoers.d/90-cloud-init-users
@@ -60,21 +59,17 @@ echo "@reboot passwd -u centos" >>/var/spool/cron/root
 chmod 600 /var/spool/cron/root
 
 ## Enable Password Logins
-sed -i -e '/^PasswordAuthentication/ c PasswordAuthentication yes' -e '/^PermitRootLogin/ c PermitRootLogin yes' /etc/ssh/sshd_config
-chattr +i /etc/ssh/sshd_config
-Stat $? "Enable Password Login"
+sed -i -e '/^PasswordAuthentication/ c PasswordAuthentication yes' -e '/^PermitRootLogin/ c PermitRootLogin yes' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/50-cloud-init.conf
+
 
 ## Setup user passwords
 ROOT_PASS="DevOps321"
 CENTOS_PASS="DevOps321"
-#usermod -a -G google-sudoers centos &>/dev/null
+
 echo "echo $ROOT_PASS | passwd --stdin root"   >>/etc/rc.d/rc.local 
 echo "echo $CENTOS_PASS | passwd --stdin centos"   >>/etc/rc.d/rc.local 
 echo "sed -i -e 's/^centos:!!/centos:/' /etc/shadow" >>/etc/rc.d/rc.local
-Stat $? "Setup Password for Users"
-info "   Following are the Usernames and Passwords"
-Infot "centos / $CENTOS_PASS"
-Infot "  root / $ROOT_PASS"
+
 echo
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIfSCB5MtXe54V3lWGBGSxMWPue5CjmSA4ky7E8GUoeZdXxI+df7msJL93PzmtwU3v+O+NLNJJRfmaGpEkgidVXoi6mnYUVCHb1y4zd6QIFEyglGDlvZ4svhHt7T15B13bJC3mTaR2A/xqlvE0/a4XKN1ATYyn6K6CTFJT8I4TIDQmO3PbcNsNFXoO1ef657aqNf0AXC1QWum3HulIt6iJ4s0pQI4hDTmR5EskJxr2K62F4JDOYmVu8bGhFT6ohYbXBCGQtmdp716RnF0Cp1htmxM001wvCSjWLPZuuBjtHXX+op+MJGr0aIqqxdVZ2gw0JeIDfVo7pkSIdTu+p2Yn devops' >/root/.ssh/authorized_keys
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFoOQSSWSX4iJ1F42FODfS7Ct7wxnzRMuKAoTK67Zd5JkjETvroEOcwJHKeRVbjLT8hZuWMz3JdowR25+7W5N23GaBvBq7HbQwec2UGGA6AFAMmijpY1KDZznfBsqVvMY5yT/4XB1RU78dffRuNUs/IeMYnxoh6UO62Zg33JLtJY6waIFNtCFPTN8m4JrsPlt4s6X8E15Jn9Qh9TDNw+R7piDZ/KRDE+paMkflMpptfcNIbK8kzC9/p3DiAMBjmfrReGueI9vrSN66L/BepPTRoUvv9iavKbmu8DEITETlhGnn79V0r0ekXDE6WgZtnTBbbjSFsilNmLw7xjGMS0Bx root@ip-172-31-15-115.ec2.internal' >>/root/.ssh/authorized_keys
@@ -86,7 +81,7 @@ systemctl enable rc-local
 sed -i -e '4 i colorscheme desert' /etc/vimrc
 
 echo 'centos ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/centos
-
+chattr +i /etc/ssh/sshd_config /etc/ssh/sshd_config.d/50-cloud-init.conf /etc/sudoers.d/centos
 
 cp /home/ec2-user/aws-image-devops-session/centos-9/scripts /etc/motd
 
